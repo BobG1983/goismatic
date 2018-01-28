ba2038885811dd50f82ba56f24f49d32c352227a
Bob Gardner <rantingbob@gmail.com>
2018-01-28 01:56:00 -0800

    Working client

--- goismatic.go
+++ goismatic.go
@@ -1,15 +1,74 @@
 package goismatic
 
-// Lang represents the languages supported by this library
-type Lang int
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"time"
+)
 
-// List of supported languages
+type lang int
+
+// The list of supported languages
 const (
-	English Lang = iota
+	English lang = iota
 	Russian
 )
 
-// Quote returns a random quote in either English or Russian from forismatic.com
-func Quote(lang Lang) string {
-	return "Party on wayne"
+var languages = [...]string{
+	"en",
+	"ru",
+}
+
+// The Quote struct represents the details of a quote from forismatic.com
+type Quote struct {
+	QuoteText   string
+	QuoteAuthor string
+}
+
+func (q *Quote) String() string {
+	return "\"" + q.QuoteText + "\" - " + q.QuoteAuthor
+}
+
+const apiURL string = "http://api.forismatic.com/api/"
+const apiVer string = "1.0"
+
+// Get returns a random quote in either English or Russian from forismatic.com
+func Get(l lang) (*Quote, error) {
+	q := new(Quote)
+
+	URL := apiURL + apiVer + "/"
+
+	req, err := http.NewRequest("GET", URL, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	qu := req.URL.Query()
+	qu.Add("method", "getQuote")
+	qu.Add("format", "json")
+	qu.Add("lang", languages[l])
+	req.URL.RawQuery = qu.Encode()
+
+	client := &http.Client{
+		Timeout: time.Second * 10,
+	}
+
+	resp, err := client.Do(req)
+
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return q, errors.New(resp.Status)
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(q)
+	if err != nil {
+		return q, err
+	}
+
+	if q.QuoteAuthor == "" {
+		q.QuoteAuthor = "Unknown"
+	}
+
+	return q, nil
 }
